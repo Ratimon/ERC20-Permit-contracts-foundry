f5d0aaf11a99765b93119546923d52c7211d9677
Ratimon <ratixoxo@gmail.com>
2023-07-16 21:46:03 +0700

    Feat & Build (./src): Adding deploy script && generate schema

---
+++ script/000_DeployEIP721Token.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.15;
+
+import {DeployScript, Deployer} from "forge-deploy/DeployScript.sol";
+import {DeployerFunctions} from "@generated/deployer/DeployerFunctions.g.sol";
+
+import {TestToken} from "@main/EIP721Token.sol";
+
+contract DeployTestTokenScript is DeployScript {
+    using DeployerFunctions for Deployer;
+
+    address owner;
+
+    function deploy() external returns (TestToken) {
+        string memory mnemonic = vm.envString("MNEMONIC");
+        uint256 ownerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+        owner = vm.envOr("DEPLOYER", vm.addr(ownerPrivateKey));
+
+        return TestToken(deployer.deploy_TestToken("TestToken"));
+    }
+}
