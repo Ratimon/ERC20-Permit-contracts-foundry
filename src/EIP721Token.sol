29e676a5dba4a4307659097f6913caea13c6c868
Ratimon <ratixoxo@gmail.com>
2023-07-19 10:47:12 +0700

    Feat (./src): Adding Premint

--- src/EIP721Token.sol
+++ src/EIP721Token.sol
@@ -5,5 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from  "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract TestToken is ERC20, ERC20Permit {
-    constructor() ERC20("Test", "T") ERC20Permit("Test") {}
+    constructor() ERC20("Test", "T") ERC20Permit("Test") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
 }
